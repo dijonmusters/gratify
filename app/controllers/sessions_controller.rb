3c81bd36eff3e7a7ec8bbc70704a7aaf526619c2
Jon Meyers <jonathon.d.meyers@gmail.com>
2020-07-09 20:53:26 +1000

    add basic authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  skip_before_action :authorized, only: [:new, :create, :welcome]
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+
+    if @user
+      session[:user_id] = @user.id
+      redirect_to '/welcome'
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def login
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to '/welcome'
+  end
+
+  def welcome
+  end
+end
